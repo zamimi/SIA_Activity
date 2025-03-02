4819faa1e043e6af33dfb2e69665be4b863efc06
zamimi <shanezamantha@gmail.com>
2025-03-02 21:19:08 +0800

    fixed a typo, respnse

--- laravel/ddsbe/app/Traits/ApiResponser.php
+++ laravel/ddsbe/app/Traits/ApiResponser.php
@@ -5,7 +5,7 @@ namespace App\Traits;
 use Illuminate\Http\Response;
 
 trait ApiResponser {
-    public function successResponse($data, $code = Respnse::HTTP_OK) {
+    public function successResponse($data, $code = Response::HTTP_OK) {
         return response()->json(['data' => $data], $code);
     }
 
