d33a91d5d31cbde4a72fa92effb7464c8a969f12
zamimi <shanezamantha@gmail.com>
2025-03-02 20:45:13 +0800

    frustratingly doing changes again, mainly about the database part

--- laravel/ddsbe/app/Http/Controllers/UserController.php
+++ laravel/ddsbe/app/Http/Controllers/UserController.php
@@ -1,10 +1,11 @@
 <?php
     namespace App\Http\Controllers;
+
     use Illuminate\Http\Request;
     use App\Models\User;
     use App\Traits\ApiResponser;
     use Illuminate\Http\Response;
-
+    
     Class UserController extends Controller {
         use ApiResponser;
 
@@ -39,7 +40,7 @@
             $this->validate($request,$rules);
 
             $user = User::create($request->all());
-            
+
             return $this->successResponse($user, Response::HTTP_CREATED);
         }
         /**
@@ -126,3 +127,38 @@
         }
 
     }
+
+    // AAAAAAAAAAAAA
+    /*
+<?php
+
+    namespace App\Http\Controllers;
+
+    use Illuminate\Http\Request;
+    // use Illuminate\Http\Response;
+    use App\Models\User;
+    use App\Traits\ApiReponser;
+    use DB;
+
+    Class UserController extends Controller {
+        use ApiResponser;
+        private $request;
+        public function __construct(Request $request){
+            $this->request = $request;
+        }
+        
+        public function getUsers(){
+            // $users = User::all();
+            // return response()->json(['data', $users], 200);
+
+            $users = DB::connection('mysql')
+            ->select("Select * from tbluser");
+            return $this->successResponse($users);
+        }
+
+        public function index(){
+            $users = User::all();
+            return $this->successResponse($users);
+        }
+    }
+    */
