84abaaf028c9a4ce3235fa598fef7183214cb94a
zamimi <shanezamantha@gmail.com>
2025-03-02 14:31:35 +0800

    changed the class toCustomers from User

--- laravel/ddsbe/app/Models/Customer.php
+++ laravel/ddsbe/app/Models/Customer.php
@@ -1,7 +1,7 @@
 <?php
     namespace App\Models;
     use Illuminate\Database\Eloquent\Model;
-        class User extends Model{
+        class Customer extends Model{
         protected $table = 'customer_t';
         // column sa table
         protected $fillable = [
