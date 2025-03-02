08e305fbd3f0a8dfc7e15099fc6a13ae062f42be
zamimi <shanezamantha@gmail.com>
2025-03-02 15:15:22 +0800

    Fixed line issue and ended up removing a fiew lines

--- laravel/ddsbe/app/Http/UserController.php
+++ laravel/ddsbe/app/Http/UserController.php
@@ -1,4 +1,5 @@
 <?php
+
     namespace App\Http\Controllers;
     use Illuminate\Http\Request;
     use App\User;
@@ -8,6 +9,7 @@
         public function __construct(Request $request){
             $this->request = $request;
         }
+        
         public function getUsers(){
             $users = User::all();
             return response()->json($users, 200);
