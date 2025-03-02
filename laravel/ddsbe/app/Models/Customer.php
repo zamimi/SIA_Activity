112873a82f6101d1b9874a8579086c5ca7468f4e
zamimi <shanezamantha@gmail.com>
2025-03-02 14:22:10 +0800

    added a Cutomer.php file to connect the database

---
+++ laravel/ddsbe/app/Models/Customer.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App;
+    use Illuminate\Database\Eloquent\Model;
+        class User extends Model{
+        protected $table = 'customer_t';
+        // column sa table
+        protected $fillable = [
+        'CustomerID', 'CustomerName', 'CustomerAddress', 'CustomerCity', 'CustomerState', 'CustomerPostalCode'
+        ];
+    }
