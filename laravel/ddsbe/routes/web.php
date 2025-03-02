97666e68ae2002d8aa4900018e27c3eb8e1ac834
zamimi <shanezamantha@gmail.com>
2025-03-02 20:04:08 +0800

    followed the pdf again

--- laravel/ddsbe/routes/web.php
+++ laravel/ddsbe/routes/web.php
@@ -18,5 +18,15 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/users',['uses' => 'UserController@getUsers']);
+   });
+
 
 $router->get('/users',['uses' => 'UserController@getUsers']);
+$router->get('/users', 'UserController@index'); // get all users records
+$router->post('/users', 'UserController@add'); // create new user record
+$router->get('/users/{id}', 'UserController@show'); // get user by id 
+$router->put('/users/{id}', 'UserController@update'); // update user record
+$router->patch('/users/{id}', 'UserController@update'); // update user record
+$router->delete('/users/{id}', 'UserController@delete'); // delete record
