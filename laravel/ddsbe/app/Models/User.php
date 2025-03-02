3e0af37af0d4e4df6c15bfb4468492ff70b6006d
zamimi <shanezamantha@gmail.com>
2025-03-03 01:14:07 +0800

    I also dont remember, changes here maybe

--- laravel/ddsbe/app/Models/User.php
+++ laravel/ddsbe/app/Models/User.php
@@ -1,8 +1,10 @@
 <?php
     namespace App\Models;
     use Illuminate\Database\Eloquent\Model;
+
     class User extends Model{
         protected $table = 'tbluser';
+        
         // column sa table
         protected $fillable = [
             'username', 'password'
