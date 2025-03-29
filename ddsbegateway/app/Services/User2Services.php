a97b5c82a1ff658821ba3cdc31338c5d3ec3cfb2
zamimi <shanezamantha@gmail.com>
2025-03-30 00:25:26 +0800

    All for the sake of services

---
+++ ddsbegateway/app/Services/User2Services.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+use App\Traits\ConsumesExternalService;
+
+class User2Service{
+
+    use ConsumesExternalService;
+
+    /**
+     * The base uri to consume the User1 Service
+     * @var string
+     */
+    public $baseUri;
+
+    public function __construct() {
+        $this->baseUri =
+        config('services.users2.base_uri');
+    }
+}
