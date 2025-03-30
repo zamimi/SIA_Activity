48689bd97a138e14ff6ccb56aea0a4eb330970f2
zamimi <shanezamantha@gmail.com>
2025-03-30 21:12:37 +0800

    spacing changes

--- ddsbe/app/Traits/ApiResponser.php
+++ ddsbe/app/Traits/ApiResponser.php
@@ -6,10 +6,10 @@ use Illuminate\Http\Response;
 
 trait ApiResponser {
     public function successResponse($data, $code = Response::HTTP_OK) {
-        return response()->json(['data' => $data], $code);
+        return response()->json(['data' => $data, 'site' => 1], $code);
     }
 
     public function errorResponse($message, $code) {
-        return response()->json(['error' => $message, 'code' => $code], $code);
+        return response()->json(['error' => $message, 'site' => 1, 'code' => $code], $code);
     }
 }
--- ddsbe2/app/Traits/ApiResponser.php
+++ ddsbe2/app/Traits/ApiResponser.php
@@ -6,10 +6,10 @@ use Illuminate\Http\Response;
 
 trait ApiResponser {
     public function successResponse($data, $code = Response::HTTP_OK) {
-        return response()->json(['data' => $data], $code);
+        return response()->json(['data' => $data, 'site' => 2], $code);
     }
 
     public function errorResponse($message, $code) {
-        return response()->json(['error' => $message, 'code' => $code], $code);
+        return response()->json(['error' => $message, 'site' => 2, 'code' => $code], $code);
     }
 }
