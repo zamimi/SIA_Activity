bf1c191bf69a015dc5f0802ce7c1fbb68d4dfc28
zamimi <shanezamantha@gmail.com>
2025-04-12 17:57:39 +0800

    added an empty database in ddsbegateway

--- ddsbegateway/bootstrap/app.php
+++ ddsbegateway/bootstrap/app.php
@@ -94,8 +94,10 @@ $app->configure('app');
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
